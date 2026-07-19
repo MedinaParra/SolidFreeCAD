7f975465b097a1b589345bf33af51038fb968dca
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-19 08:44:41 -0400

    Bump native preview to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.aistudio.solidmacro.fctech"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "1.1-native-preview"
+    versionCode = 3
+    versionName = "1.2-step-opengl"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
